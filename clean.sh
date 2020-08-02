f3a2afd9cbcf76203c2bea5352954d54de931253
zusterben <zusterben1s@hotmail.com>
2020-08-02 13:10:38 +0800

    add clean.sh

---
+++ clean.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+rm -rf openssl-1.1.1d
+rm -rf zlib-1.2.11
+rm -rf boost_1_71_0
+rm -rf libevent-2.1.11-stable
+rm -rf libev-4.22
+rm -rf pcre-8.41
+rm -rf c-ares-1.14.0
+rm -rf mbedtls-2.16.3
+rm -rf chinadns-ng
+rm -rf dns2socks
+rm -rf ipt2socks-1.1.3
+rm -rf jq
+rm -rf libsodium-1.0.16
+rm -rf httping-2.5
+rm -rf lua-5.1.5
+rm -rf microsocks.1.0.1
+rm -rf pdnsd-1.2.9b-par
+rm -rf redsocks2-0.67
+rm -rf microsocks
+rm -rf shadowsocks-libev-3.3.4
+rm -rf simple-obfs
+rm -rf trojan-1.16.0
+rm -rf shadowsocksr-libev-2.5.3
+
